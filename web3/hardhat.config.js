c1e35116f728cd068cd961899c7b203e63a14464
Sumanjit38 <sumanjit38@gmail.com>
2023-05-04 21:32:38 +0530

    update

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaultNetwork: 'Ganache',
+    networks: {
+      hardhat: {},
+      Ganache: {
+        url: 'HTTP://127.0.0.1:7545',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
